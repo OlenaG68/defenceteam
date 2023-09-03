b7630340dbc8afc37c63a6cff7a1b7c9b90ed00a
Olena Hrynykha <olenagrynyha@gmail.com>
2023-09-03 17:46:02 +0300

    fix btn color

--- components/UI/Button.tsx
+++ components/UI/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Button = () => {
     return (
-        <button className="bg-blue hover:bg-cyan-700 px-[25px] py-[18px] rounded-xl mt-24 text-white font-semibold">
+        <button className="bg-blue hover:bg-[#152fa3] px-[25px] py-[18px] rounded-xl mt-24 text-white font-semibold">
             Замовити консультацію
         </button>
     );
